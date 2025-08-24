702c51d2751ea01666f115401e1a6b5c05a31d49
dylan7474 <112658365+dylan7474@users.noreply.github.com>
2025-08-24 17:53:34 +0100

    Fix main signature for SDL2 on Windows

--- main.c
+++ main.c
@@ -437,7 +437,7 @@ void reset_game(void) {
     init_wave(1);
 }
 
-int main(void) {
+int main(int argc, char *argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 1;
